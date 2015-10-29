800fdf7a3208dc5712e9782dd0900cbb5a9107a7
Ignat <iob.veritas@gmail.com>
2015-10-29 22:35:16 +0200

    add adjList

--- app/client/pages/index/index.js
+++ app/client/pages/index/index.js
@@ -5,18 +5,63 @@ import MapService from '../../services/map'
 import Map from '../../components/map'
 import Menu from '../../components/menu'
 
+function getDist(a, b){
+  let R = 6371000,
+      f1 = a.lat*Math.PI/180,
+      f2 = b.lat*Math.PI/180,
+      df = f2 - f1,
+      dl = (b.lng - a.lng)*Math.PI/180,
+      hrsin = Math.sin(df/2)*Math.sin(df/2) + Math.cos(f1)*Math.cos(f2)*Math.sin(dl/2)*Math.sin(dl/2)
+
+  return 2 * R * Math.atan2(Math.sqrt(hrsin), Math.sqrt(1 - hrsin))
+}
+
+function getAdjList(areas){
+  if (!areas) return null
+
+  let adjList = {}
+
+  areas.forEach((area)=>{
+    let arr = [...area.towers, ...area.bgps]
+    arr.forEach((a)=>{
+      adjList[a._id] = arr.filter((b)=>{
+        return a._id != b._id && getDist(a, b) <= a.radius
+      }).map((b)=>{
+        return b._id
+      })
+    })
+  })
+  debugger
+  areas.reduce((arr, area)=>{
+    return arr.concat(area.bgps)
+  }, []).forEach((a)=>{
+    let arr = areas.filter((b)=>{
+      return a.area != b.area && getDist(a, b) <= a.radius
+    })
+    adjList[a._id].push(...arr)
+  })
+
+  return adjList
+}
 
 const Index = React.createClass({
   mixins: [PureRenderMixin, SameMixin],
 
   getInitialState() {
-    return {areas: MapService.areas || []}
+    return {
+      areas: MapService.areas || [],
+      adjList: getAdjList(MapService.areas) || {}
+    }
   },
 
   componentDidMount() {
     if (!this.state.length) {
       MapService.onLoad((areas) => {
-        this.setState({areas})
+        let adjList = getAdjList(areas)
+        this.setState({
+          areas,
+          adjList
+        })
       })
     }
   },
@@ -25,7 +70,7 @@ const Index = React.createClass({
     return (
       <div className='content'>
         <Menu />
-        <Map areas={this.state.areas}/>
+        <Map areas={this.state.areas} adjList={this.state.adjList}/>
       </div>
     )
   }
