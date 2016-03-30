3a992a8146ca5ae613e4f67fdbca7b7e949eff4c
A-gambit <a.gambit.gregory@gmail.com>
2016-03-30 21:11:03 +0300

    fix npm

--- app/client/index.js
+++ app/client/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {render} from 'react-dom'
-import {Router, Route, IndexRoute} from 'react-router'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import Index from './pages/index'
 import App from './pages/app'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
 const router = (
-  <Router>
+  <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Index} />
     </Route>
